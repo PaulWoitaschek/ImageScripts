b8ddf86356fc34282890e0a4b4e29d93c9f6c4d0
Paul Woitaschek <woitaschek@posteo.de>
2018-08-06 22:15:50 +0200

    Parse the arguments for svg2webp

--- svg2webp.py
+++ svg2webp.py
@@ -1,17 +1,25 @@
+import argparse
 
-
-import glob
 import os
 from os import path
 from pathlib import Path
 from subprocess import call
 
-for file in glob.glob("*.svg"):
+
+def resize(file: str):
     dir_name = path.dirname(file)
-    resized_png = path.join(dir_name, "temp.png")
+    png_resized = path.join(dir_name, "temp.png")
     try:
-        call(["rsvg-convert", "-a", "-f", "png", file, "-w", "2048", "-h", "2048", "-o", resized_png])
+        call(["rsvg-convert", "-a", "-f", "png", file, "-w", "2048", "-h", "2048", "-o", png_resized])
         output = Path(file).with_suffix(".webp")
-        call(["cwebp", resized_png, "-lossless", "-o", output])
+        call(["cwebp", png_resized, "-lossless", "-o", output])
     finally:
-        os.remove(resized_png)
+        os.remove(png_resized)
+
+
+parser = argparse.ArgumentParser(description='Convert SVG to WebP')
+parser.add_argument('--input', required=True, type=str, help="The image you want to convert")
+args = parser.parse_args()
+if not os.path.exists(args.input):
+    parser.error("Image does not exist.")
+resize(args.input)
