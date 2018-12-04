6d824a58b74988afea7c322e4500ed00dec6aecd
Paul Woitaschek <woitaschek@posteo.de>
2018-12-04 12:02:28 +0100

    Handle creating android subdirectory images

--- svg2webp.kts
+++ svg2webp.kts
@@ -1,46 +1,115 @@
 #!/usr/bin/env kscript
-@file:DependsOn("org.apache.commons:commons-exec:1.3", "com.beust:jcommander:1.71")
+@file:DependsOn("org.apache.commons:commons-exec:1.3")
+@file:DependsOn("info.picocli:picocli:3.8.1")
 
-import com.beust.jcommander.JCommander
-import com.beust.jcommander.Parameter
-import org.apache.commons.exec.CommandLine
+
 import org.apache.commons.exec.DefaultExecutor
+import picocli.CommandLine
+import picocli.CommandLine.Command
+import picocli.CommandLine.Option
+import picocli.CommandLine.ParameterException
+import picocli.CommandLine.Parameters
+import picocli.CommandLine.Spec
 import java.io.File
+import kotlin.math.roundToInt
 
-data class Args(
-  @Parameter(description = "Files")
-  var files: List<String> = ArrayList(),
-  @Parameter(names = ["--help"], help = true)
-  var help: Boolean = false
+
+enum class AndroidImageSize(val factor: Double) {
+  MDPI(1.0),
+  HDPI(1.5),
+  XHDPI(2.0),
+  XXHDPI(3.0),
+  XXXHDPI(4.0)
+}
+
+
+@Command(
+  name = "Image Converter",
+  mixinStandardHelpOptions = true
 )
+class MyApp : Runnable {
 
-val arguments = Args()
-val jCommander = JCommander.newBuilder().addObject(arguments).build()!!
-  .also {
-    it.parse(*args)
+  @Spec
+  lateinit var spec: CommandLine.Model.CommandSpec
+
+  @Parameters(description = ["The files to convert"])
+  private var inputFiles: List<File> = mutableListOf()
+
+  @Option(names = ["-a", "--android"])
+  private var isAndroid: Boolean = false
+
+  @Option(names = ["-dp"])
+  private var heightInDp: Int? = null
+
+  private val executor = DefaultExecutor()
+
+  private fun String.execute() {
+    executor.execute(org.apache.commons.exec.CommandLine.parse(this))
   }
-run(arguments)
 
-fun run(arguments: Args) {
-  if (arguments.help) {
-    jCommander.usage()
-    return
+  private val tmpDir = File("/tmp/image_script").apply {
+    deleteRecursively()
+    mkdirs()
   }
 
-  val executor = DefaultExecutor()
-  arguments.files
-    .map(::File)
-    .toSet()
-    .forEach { input ->
-      require(input.exists() && input.extension == "svg") {
-        "File $input must exist and be a svg file."
+  override fun run() {
+    if (isAndroid) {
+      createAndroidImages()
+    } else {
+      createSingleLargeImages()
+    }
+  }
+
+  private fun createSingleLargeImages() {
+    inputFiles.forEach { input ->
+      if (input.extension != "svg") {
+        throw ParameterException(spec.commandLine(), "File $input is no svg file")
       }
-      val png = File(input.parentFile, input.nameWithoutExtension + ".png").apply {
+      val pngTmp = File(tmpDir, "image.png").apply {
+        delete()
         deleteOnExit()
       }
-      executor.execute(CommandLine.parse("inkscape ${input.absolutePath} -e ${png.absolutePath} --without-gui -h 2048x"))
 
+      "inkscape ${input.absolutePath} -e ${pngTmp.absolutePath} --without-gui -h 2048x".execute()
       val webp = File(input.parentFile, input.nameWithoutExtension + ".webp")
-      executor.execute(CommandLine.parse("cwebp -lossless ${png.absolutePath} -o ${webp.absolutePath}"))
+      "cwebp -lossless ${pngTmp.absolutePath} -o ${webp.absolutePath}".execute()
     }
+  }
+
+  private fun createAndroidImages() {
+    val heightInDp = heightInDp ?: throw ParameterException(
+      spec.commandLine(),
+      "In android-mode you  must specify the height in dp too."
+    )
+
+    inputFiles.forEach { input ->
+      if (input.extension != "svg") {
+        throw ParameterException(spec.commandLine(), "File $input is no svg file")
+      }
+      "svgo ${input.absolutePath}".execute()
+      val pngTmp = File(tmpDir, "image.png").apply {
+        delete()
+        deleteOnExit()
+      }
+      val losslessWebp = File(tmpDir, "lossless.webp")
+      val lossyWebp = File(tmpDir, "lossy.webp")
+      AndroidImageSize.values().forEach {
+        lossyWebp.delete()
+        losslessWebp.delete()
+        val heightPx = (heightInDp * it.factor).roundToInt()
+        "rsvg-convert ${input.absolutePath} -h $heightPx -o ${pngTmp.absolutePath}".execute()
+        "cwebp -short -lossless ${pngTmp.absolutePath} -o ${losslessWebp.absolutePath}".execute()
+        "cwebp -short ${pngTmp.absolutePath} -o ${lossyWebp.absolutePath}".execute()
+        val pickLossless = losslessWebp.length() < lossyWebp.length()
+        val targetWebp = if (pickLossless) losslessWebp else lossyWebp
+        val webp = File("drawable-${it.name.toLowerCase()}", input.nameWithoutExtension + ".webp").apply {
+          delete()
+          parentFile.mkdirs()
+        }
+        targetWebp.copyTo(webp)
+      }
+    }
+  }
 }
+
+CommandLine.run(MyApp(), *args)
